d33b90d78fb7532b57cce249699f994e80ded668
John Henry Galino <github@jhgalino.com>
2025-11-07 12:51:12 +0000

    Install gnome

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,9 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+# dnf5 install -y tmux 
+dnf5 groupinstall -y "GNOME Desktop Environment"
+dnf5 install -y podman distrobox
 
 # Use a COPR Example:
 #
@@ -21,4 +23,4 @@ dnf5 install -y tmux
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+# systemctl enable podman.socket
