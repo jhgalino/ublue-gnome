6ddc9238e26e325aa5788f5a69c56fdb9de507b2
John Henry Galino <github@jhgalino.com>
2025-11-07 14:15:57 +0000

    fix command

--- build_files/build.sh
+++ build_files/build.sh
@@ -11,7 +11,7 @@ set -ouex pipefail
 
 # this installs a package from fedora repos
 # dnf5 install -y tmux 
-dnf5 group install -y gnome-tweaks
+dnf5 install -y gnome-tweaks
 
 # Use a COPR Example:
 #
